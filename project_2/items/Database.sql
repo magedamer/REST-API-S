ff331aa0f0354d2dae8f5f63a31c6793a25cb6f3
maged amer <magedamer44@gmail.com>
2022-03-21 22:16:43 +0200

    first commit

---
+++ project_2/items/Database.sql
@@ -0,0 +1,9 @@
+CREATE TABLE `items` (
+  `id` int(11) NOT NULL,
+  `name` varchar(256) NOT NULL,
+  `description` text NOT NULL,
+  `price` int(255) NOT NULL,
+  `category_id` int(11) NOT NULL,
+  `created` datetime NOT NULL,
+  `modified` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
